ac1ad36d71b96fdf217e63a059855623a854f5ff
joabe da conceição <joabeercg2012@gmail.com>
2025-08-14 19:01:30 +0000

    desafio_xadrez

---
+++ desafio_xadrez.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    // Mover a torre 5 casas para a direita
+     for (int i = 0; i < 5; i++)
+      {
+     printf("Direita\n"); // Imprime a direção do movimento
+      }
+
+    // Mover o bispo 5 casas na diagonal para cima á direita
+
+   // int i = 0;
+
+  //  while (i < 5)
+  //  {
+
+    //    printf("Para cima á direita\n", i);
+
+      //  i++;
+//    }
+
+    // Mover a rainha 8 casas á esquerda
+    // int i = 0;
+
+    // do
+    // {
+    //  printf("Esquerda\n", i);
+    //  i++;
+    // } while (i < 8);
+
+    return 0;
+}
